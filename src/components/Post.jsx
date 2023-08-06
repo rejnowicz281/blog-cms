d4d9d772c4a2c4fc77d60dfd7c7822dafb75f260
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-06 13:48:39 +0200

    Remove console.log in post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,6 @@ import { getPost } from "../../helpers/API";
 
 function Post() {
     const { id } = useParams();
-
-    console.log(id);
     return (
         <div>
             <button onClick={async () => console.log(await getPost(id))}>Click to console log post</button>
