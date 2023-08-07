8874237a325752935d1ae6b436191c2e0ac96581
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-07 15:49:49 +0200

    Validate token on App mount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
+import jwt_decode from "jwt-decode";
+import { useEffect, useState } from "react";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Post from "./Post";
 import Posts from "./Posts";
 
 function App() {
+    const [authenticated, setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            const decodedToken = jwt_decode(token);
+            const currentTime = Date.now() / 1000;
+
+            if (decodedToken.exp < currentTime) {
+                localStorage.removeItem("token");
+                setAuthenticated(false);
+            } else {
+                setAuthenticated(true);
+            }
+        }
+    }, []);
+
     return (
         <HashRouter>
             <Routes>
-                {localStorage.getItem("token") ? (
+                {authenticated ? (
                     <>
                         <Route path="/*" element={<Navigate to="/blog-cms/posts" />} />
                         <Route path="/blog-cms/posts" element={<Posts />} />
@@ -16,7 +36,7 @@ function App() {
                 ) : (
                     <>
                         <Route path="/*" element={<Navigate to="/blog-cms/login" />} />
-                        <Route path="/blog-cms/login" element={<Login />} />
+                        <Route path="/blog-cms/login" element={<Login setAuthenticated={setAuthenticated} />} />
                     </>
                 )}
             </Routes>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,28 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 import { login } from "../../helpers/API";
 
-function Login() {
+function Login({ setAuthenticated }) {
+    const [error, setError] = useState(null);
     const [password, setPassword] = useState("");
 
     async function handleSubmit(e) {
         e.preventDefault();
-        await login(password);
+        const res = await login(password);
+
+        const token = res.data.token;
+
+        if (res.status === 200 && token) {
+            localStorage.setItem("token", token);
+            setAuthenticated(true);
+        } else {
+            setError(res.data.error.message);
+        }
     }
 
     return (
         <div>
+            {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
@@ -20,5 +32,9 @@ function Login() {
         </div>
     );
 }
+
+Login.propTypes = {
+    setAuthenticated: PropTypes.func.isRequired,
+};
 
 export default Login;
