020a491c0746bb1a8aebdf9ff41db9c316b514b0
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-08 18:28:08 +0200

    Add token for getPosts request

--- helpers/API.js
+++ helpers/API.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 export async function getPosts() {
     try {
-        const response = await axios.get("http://localhost:3000/posts");
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:3000/posts", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
 
         return response;
     } catch (error) {
