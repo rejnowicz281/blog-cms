7beb3c2634b601d8434d2ca2bff3adaf4ddb9a3d
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-09 11:14:18 +0200

    Update API_URL

--- helpers/API.js
+++ helpers/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://blog-api-prod.fly.dev/";
 
 const apiAuthorized = axios.create({
     baseURL: API_URL,
